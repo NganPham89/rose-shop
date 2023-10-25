ea2a97b002c130226ef8a25fd325285cb22d4c9f
NganPham89 <algarvian2001@yahoo.com>
2023-10-25 17:12:46 -0400

    added update/delete in store setting

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -1,6 +1,10 @@
 "use client";
 
 import * as z from "zod";
+import toast from "react-hot-toast";
+import axios from "axios";
+import { useParams, useRouter } from "next/navigation";
+
 import { Button } from "@/components/ui/button";
 import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
@@ -11,6 +15,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useState } from "react";
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { AlertModal } from "@/components/modals/alert-modal";
 
 interface SettingsFormProps {
     initialData: Store;
@@ -24,6 +29,9 @@ type SettingsFormValues = z.infer<typeof formSchema>;
 
 export const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
 
+    const params = useParams();
+    const router = useRouter();
+
     const [open, setOpen] = useState(false);
     const [loading, setLoading] = useState(false);
 
@@ -33,11 +41,42 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
     })
 
     const onSubmit = async (data: SettingsFormValues) => {
-        console.log(data)
+        // console.log(data)
+        try {
+            setLoading(true);
+            await axios.patch(`/api/stores/${params.storeId}`, data)
+            router.refresh();
+            toast.success("Store updated.");
+        } catch (error) {
+            toast.error("Something went wrong.")
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    const onDelete = async () => {
+        try {
+            setLoading(true);
+            await axios.delete(`/api/stores/${params.storeId}`)
+            router.refresh();
+            router.push("/");
+            toast.success("Store deleted.")
+        } catch (error) {
+            toast.error("Make sure you remove all products and categories first.")
+        } finally {
+            setLoading(false);
+            setOpen(false);
+        }
     }
 
     return (
         <>
+            <AlertModal
+                isOpen={open}
+                onClose={() => setOpen(false)}
+                onConfirm={onDelete}
+                loading={loading}
+            />
             <div className="flex items-center justify-between">
                 <Heading
                     title="Settings"
@@ -56,15 +95,15 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
             <Form {...form}>
                 <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 w-full">
                     <div className="grid grid-cols-3 gap-8">
-                        <FormField control={form.control} name="name" render={({field}) => (
+                        <FormField control={form.control} name="name" render={({ field }) => (
                             <FormItem>
                                 <FormLabel>Name</FormLabel>
                                 <FormControl>
-                                    <Input disabled={loading} placeholder="Store name" {...field}/>
+                                    <Input disabled={loading} placeholder="Store name" {...field} />
                                 </FormControl>
                                 <FormMessage />
                             </FormItem>
-                        )}/>
+                        )} />
                     </div>
                     <Button disabled={loading} className="ml-auto" type="submit">
                         Save changes
--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -79,7 +79,7 @@ export const StoreModal = () => {
                     </Form>
                 </div>
             </div>
-            Future Create Store Form
+            {/* Future Create Store Form */}
         </Modal>
     )
 }
