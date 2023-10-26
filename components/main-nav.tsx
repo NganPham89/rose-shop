8d700c9e2f2276d756fffe236752c858c8e9de8b
NganPham89 <algarvian2001@yahoo.com>
2023-10-26 12:12:07 -0400

    set up image upload with cloudinary

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -12,10 +12,20 @@ export function MainNav({
     const params = useParams();
     const routes = [
         {
+            href: `/${params.storeId}`,
+            label: "Overview",
+            active: pathname === `/${params.storeId}`
+        },
+        {
+            href: `/${params.storeId}/billboards`,
+            label: "Billboards",
+            active: pathname === `/${params.storeId}/billboards`
+        },
+        {
             href: `/${params.storeId}/settings`,
             label: "Settings",
             active: pathname === `/${params.storeId}/settings`
-        }
+        },
     ];
     return (
         <nav
